7aac5fca3b45cf7fa5a97a1eab11a54d52d6b0ec
Juanda <juandavid.jdlea@gmail.com>
2023-09-30 18:27:06 -0500

    interfaces

--- AdminHierritos/src/main/java/interfaces/RMIAdmin.java
+++ AdminHierritos/src/main/java/interfaces/RMIAdmin.java
@@ -5,9 +5,7 @@ import clases.EmpresaProveedora;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface RMIAlmacen extends Remote {
+public interface RMIAdmin extends Remote {
     public boolean iniciarSesion(String usuario, String contrasena) throws RemoteException;
-
-    public EmpresaProveedora buscarProveedor(String nitONombre) throws RemoteException;
 
 }
