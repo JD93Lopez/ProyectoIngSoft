22131cfe64a28690724912cfe8c1343922fc04f6
Juanda <juandavid.jdlea@gmail.com>
2023-10-03 18:57:07 -0500

    TarjetasBusquedaProveedor

--- AlmacenHierritos/src/main/java/com/example/almacenhierritos/BuscarProveedorController.java
+++ AlmacenHierritos/src/main/java/com/example/almacenhierritos/BuscarProveedorController.java
@@ -2,7 +2,9 @@ package com.example.almacenhierritos;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -18,9 +20,10 @@ public class BuscarProveedorController {
 
     static Scene scene;
     static VBox tarjeta;
-
     @FXML
     GridPane gridPane;
+    @FXML
+    ScrollPane scrollPane;
     @FXML
     TextField textfieldBuscar;
     @FXML
@@ -36,17 +39,59 @@ public class BuscarProveedorController {
         Main.mainStage.setScene(MenuController.scene);
     }
 
+    private int col=0;
+    private int fil=1;
+    private int i=0;
+
     public void insertarTarjeta(){
+        //SI HAY MENOS DE 9 Productos debe empezar en la fila 0
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("tarjetaProducto.fxml"));
-            try {
-                tarjeta = fxmlLoader.load();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            TarjetaController tarjetaController = fxmlLoader.getController();
-            tarjetaController.setLabelNombreProducto("NOMBREEE");
-            gridPane.getChildren().add(tarjeta);
+        try {
+            tarjeta = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TarjetaController tarjetaController = fxmlLoader.getController();
+        tarjetaController.setLabelNombreProducto("NOMBREEE");
+        tarjetaController.setLabelDescProducto(fil+", "+col);
+        gridPane.add(tarjeta, col++, fil);
+        GridPane.setMargin(tarjeta,new Insets(10));
+        if (col == 3) {
+            col = 0;
+            fil++;
+        }
     }
+
+/*    public void insertarTarjeta(){
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("tarjetaProducto.fxml"));
+        try {
+            tarjeta = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        TarjetaController tarjetaController = fxmlLoader.getController();
+        tarjetaController.setLabelNombreProducto("NOMBREEE");
+        tarjetaController.setLabelDescProducto(fil+", "+col);
+        if( fil>3 && col==0 ){
+            gridPane.addRow(3);
+            gridPane.add(tarjeta, col++, fil);
+            GridPane.setMargin(tarjeta, new Insets(5));
+        }else {
+            gridPane.add(tarjeta, col++, fil);
+            GridPane.setMargin(tarjeta, new Insets(5));
+        }
+        gridPane.setTranslateY(40);
+        if (i == 3) {
+            gridPane.setVgap(200);
+            gridPane.setTranslateY(100);
+        } else {
+            i++;
+        }
+        if (col == 3) {
+            col = 0;
+            fil++;
+        }
+    }*/
 
 /*    public void insertarTarjeta(){
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("tarjetaProducto.fxml"));
