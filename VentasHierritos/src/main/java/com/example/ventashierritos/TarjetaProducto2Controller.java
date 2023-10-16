43197941019aec8dd47ee13b7630d213c4086809
Juanda <juandavid.jdlea@gmail.com>
2023-10-16 10:43:21 -0500

    Controllers Ventas

--- VentasHierritos/src/main/java/com/example/ventashierritos/TarjetaProducto2Controller.java
+++ VentasHierritos/src/main/java/com/example/ventashierritos/TarjetaProducto2Controller.java
@@ -1,9 +1,11 @@
 package com.example.ventashierritos;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 
 public class TarjetaProducto2Controller {
+    static Scene scene;
     @FXML
     Label labelNomProducto;
     @FXML
