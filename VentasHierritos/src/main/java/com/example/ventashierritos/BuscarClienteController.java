6c3bdd213c5301824f054b051ec03426ab1da445
Juanda <juandavid.jdlea@gmail.com>
2023-10-16 11:45:13 -0500

    textArea busca cliente Ventas

--- VentasHierritos/src/main/java/com/example/ventashierritos/BuscarClienteController.java
+++ VentasHierritos/src/main/java/com/example/ventashierritos/BuscarClienteController.java
@@ -3,12 +3,15 @@ package com.example.ventashierritos;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class BuscarClienteController {
 
     static Scene scene;
 
+    @FXML
+    TextArea textArea;
     @FXML
     TextField textfieldBuscar;
 
