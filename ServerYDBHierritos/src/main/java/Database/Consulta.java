e15780f5e183b9ae483e5a6e1742d5f4ebe9ab50
Juanda <juandavid.jdlea@gmail.com>
2023-10-15 21:32:30 -0500

    Ventas FXML

--- ServerYDBHierritos/src/main/java/Database/Consulta.java
+++ ServerYDBHierritos/src/main/java/Database/Consulta.java
@@ -1,6 +1,5 @@
 package Database;
-import clases.Cliente;
-import clases.Persona;
+import clases.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -104,6 +103,167 @@ public class Consulta {
                 e.printStackTrace();
             }
             return cliente;
+        }
+
+    }
+
+    public static Cliente obtenerClientePorId(String idcliente) {
+        Cliente cliente  = new Cliente();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conectar();
+
+            String sql = "SELECT * FROM clientes WHERE id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,idcliente);
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                cliente.setNombres(resultSet.getString("nombres"));
+                cliente.setDireccion(resultSet.getString("direccion"));
+                cliente.setCorreo(resultSet.getString("correo"));
+                cliente.setTelefono(resultSet.getString("telefono"));
+                cliente.setNumDocumento(resultSet.getString("numDocumento"));
+                cliente.setClienteFrecuente(resultSet.getBoolean("cliente frecuente"));
+                cliente.setTipoDocumento(Enum.valueOf(Persona.TipoDocumento.class,resultSet.getString("tipoDocumento")));
+                cliente.setTipoPersona(Enum.valueOf(Cliente.TipoPersona.class,resultSet.getString("tipoPersona")));
+                cliente.setResponsableDeIva(resultSet.getBoolean("responsableDeIva"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return cliente;
+        }
+
+    }
+
+    public static EmpresaProveedora obtenerEmpresaProveedoraPorId(String idcliente) {
+        EmpresaProveedora empresaProveedora  = new EmpresaProveedora();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conectar();
+
+            String sql = "SELECT * FROM empresas_proveedoras WHERE id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,idcliente);
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                empresaProveedora.setNombre(resultSet.getString("nombre"));
+                empresaProveedora.setNit(resultSet.getString("nit"));
+                empresaProveedora.setBanco(resultSet.getString("banco"));
+                empresaProveedora.setCuentaBancaria(resultSet.getString("cuentaBancaria"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return empresaProveedora;
+        }
+
+    }
+
+    public static FacturaCompra obtenerFacturaCompraPorId(String idfacturaDeCompra) {
+        FacturaCompra facturaCompra  = new FacturaCompra();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conectar();
+
+            String sql = "SELECT * FROM facturas_de_compra WHERE id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,idfacturaDeCompra);
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                facturaCompra.setNombreVendedor(resultSet.getString("nombreVendedor"));
+//              facturaCompra.setFormaDePago(resultSet.getString("formaDePago"));
+//              facturaCompra.setFechaYHora(resultSet.getString("fechaYHora"));
+                facturaCompra.setTotal(Double.parseDouble(resultSet.getString("total")));
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return facturaCompra;
+        }
+
+    }
+
+    public static FacturaVenta obtenerFacturaVentaPorId(String idfacturaDeVenta) {
+        FacturaVenta facturaVenta  = new FacturaVenta();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            conectar();
+
+            String sql = "SELECT * FROM empresas_proveedoras WHERE id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,idfacturaDeVenta);
+            resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                facturaVenta.setFechaYHora(resultSet.getString("fechaYHora"));
+                facturaVenta.setConsecutivoDian(Integer.parseInt(resultSet.getString("consecutivoDian")));
+                facturaVenta.setFormaDePago(Enum.valueOf(EmpresaProveedora.FormaDePago.class,resultSet.getString("formaDePago")));
+                facturaVenta.setTotal(Double.parseDouble(resultSet.getString("cuentaBancaria")));
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        } finally {
+
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return facturaVenta;
         }
 
     }
